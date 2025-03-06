4d04ce39164e560449be2c5874c3885fbdea1e5a
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-03-06 09:36:16 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -22,4 +22,15 @@ resource "aws_security_group" "allow_ssh_terraform" {
     tags = {
         Name = "allow-sshh"
     }
+
+
+resource "aws_instance" "terraform" {
+
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = "terraform"
+    }
+}   
 }
