1470d979545f1a374e2fcf6156a78c181e459627
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-03-07 11:17:49 +0530

    data-types

--- data-types/data.tf
+++ data-types/data.tf
@@ -1,3 +1,20 @@
-output "ami_id" {
-    value = data.aws_ami.joindevops.id
+data "aws_ami" "joindevops" {
+
+	most_recent      = true
+	owners = ["973714476881"]
+	
+	filter {
+		name   = "name"
+		values = ["RHEL-9-DevOps-Practice"]
+	}
+	
+	filter {
+		name   = "root-device-type"
+		values = ["ebs"]
+	}
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
