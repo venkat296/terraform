e9a797574e664fb9c4404603584149c99e62b5a6
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-03-08 10:59:04 +0530

    expense

--- expense-1/ec2.tf
+++ expense-1/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform" {
-
-    ami = data.aws_ami.joindevops.id
+    count                  = length(var.instance_names)
+    ami                    = data.aws_ami.joindevops.id
     instance_type          = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
