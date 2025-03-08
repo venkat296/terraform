f25a7d76e106921c24004e4346ab17049b08a1b2
venkatadurgarao <venkatadurgaraoy@gmail.com>
2025-03-08 10:57:15 +0530

    expense

--- expense-1/data.tf
+++ expense-1/data.tf
@@ -1,19 +1,20 @@
-data "aws_ami" "ami_info" {
-  most_recent = true
-  owners      = ["973714476881"]
+data "aws_ami" "joindevops" {
 
-  filter {
-    name   = "name"
-    values = ["RHEL-9-DevOps-Practice"]
-  }
+	most_recent      = true
+	owners = ["973714476881"]
+	
+	filter {
+		name   = "name"
+		values = ["RHEL-9-DevOps-Practice"]
+	}
+	
+	filter {
+		name   = "root-device-type"
+		values = ["ebs"]
+	}
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
--- expense-1/ec2.tf
+++ expense-1/ec2.tf
@@ -1,35 +1,35 @@
-resource "aws_instance" "m" {
-  count                  = length(var.instance_names)
-  ami                    = ami-09c813fb71547fc4f
-  instance_type          = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-  tags = {
-    Name = var.instance_names[count.index]
-  }
+resource "aws_instance" "terraform" {
+
+    ami = data.aws_ami.joindevops.id
+    instance_type          = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = "terraform"
+    }
 }
 
 resource "aws_security_group" "allow_ssh_terraform" {
-  name        = "allow_sshh" #allow_ssh is already there in my account
-  description = "Allow port number 22 for SSH access"
+    name        = "allow_sshh" #allow_ssh is already there in my account
+    description = "Allow port number 22 for SSH access"
 
-  # usually we allow everything in egress
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+    # usually we allow everything in egress
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
-    ipv6_cidr_blocks = ["::/0"]
-  }
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
-  tags = {
-    Name = "allow_sshh"
-  }
+    tags = {
+        Name = "allow_sshh"
+    }
 }
